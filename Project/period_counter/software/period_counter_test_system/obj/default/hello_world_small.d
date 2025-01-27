a98c5908478a4a0d076f776192a2c74f37cb836e
Igor Parchakov <igor_pa@live.com>
2025-01-27 19:44:14 +0100

    one channel scheduler

---
+++ Project/period_counter/software/period_counter_test_system/obj/default/hello_world_small.d
@@ -0,0 +1,25 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../period_counter_test_system_bsp//HAL/inc/sys/alt_stdio.h \
+ ../period_counter_test_system_bsp//HAL/inc/io.h \
+ ../period_counter_test_system_bsp//HAL/inc/alt_types.h \
+ ../period_counter_test_system_bsp/system.h \
+ ../period_counter_test_system_bsp/linker.h \
+ ../period_counter_test_system_bsp//HAL/inc/sys/alt_irq.h \
+ ../period_counter_test_system_bsp//HAL/inc/nios2.h \
+ ../period_counter_test_system_bsp//HAL/inc/alt_types.h
+
+../period_counter_test_system_bsp//HAL/inc/sys/alt_stdio.h:
+
+../period_counter_test_system_bsp//HAL/inc/io.h:
+
+../period_counter_test_system_bsp//HAL/inc/alt_types.h:
+
+../period_counter_test_system_bsp/system.h:
+
+../period_counter_test_system_bsp/linker.h:
+
+../period_counter_test_system_bsp//HAL/inc/sys/alt_irq.h:
+
+../period_counter_test_system_bsp//HAL/inc/nios2.h:
+
+../period_counter_test_system_bsp//HAL/inc/alt_types.h:
