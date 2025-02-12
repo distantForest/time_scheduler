a8953f5f7f2fa3fb02a6cd5f8b3b399dcd8f331d
Igor Parchakov <igor_pa@live.com>
2025-02-12 20:10:48 +0100

    irq selictor initial version

--- Project/hdl/irq_selector.vhd
+++ Project/hdl/irq_selector.vhd
@@ -6,7 +6,7 @@
 -- Author     : Igor Parchakov  <igor_pa@live.com>
 -- Company    : AGSTU
 -- Created    : 2025-02-07
--- Last update: 2025-02-10
+-- Last update: 2025-02-12
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -28,14 +28,14 @@ entity irq_selector is
     height : natural := 4               -- number of input irq lines
     );
   port(
-    clk      : in std_logic
-    ; reset_n    : in std_logic
-                                        -- 
-    ; irq_in_mx     : in std_logic_vector (height - 1 downto 0)
-    ; ack_in_mx     : in std_logic_vector (height - 1 downto 0)
-    ; ack_in : std_logic
-    ; irq_out    : out std_logic
-    ; vector_out : out std_logic_vector (height - 1 downto 0)             -- 
+    clk      : in  std_logic
+  ; reset_n    : in  std_logic
+                                          -- 
+  ; irq_in_mx  : in  std_logic_vector (height - 1 downto 0)
+  ; ack_in_mx  : in  std_logic_vector (height - 1 downto 0)
+  ; ack_in     : in  std_logic
+  ; irq_out    : out std_logic
+  ; vector_out : out std_logic_vector (height - 1 downto 0)  -- 
     );
 end irq_selector;
 
@@ -46,7 +46,7 @@ architecture rtl of irq_selector is
 
 begin
 
-  sejector : process (reset_n, clk)
+  selector : process (reset_n, clk)
   begin
     if reset_n = '0' then
       vector = 0;
@@ -69,8 +69,22 @@ begin
         irq_out <= '0';
         vector  <= 0;
       end if;
+      -- manage sent flag
+      for i in range 0 to height -1 loop
+        if (ack_in_mx(i) = '1') then
+          irq_sent(i) <= '1';
+          end if;
+      end loop;
     end if;
   end if;
 end process selector;
+
+acknowlege : process (reset_n, clk)
+  begin
+    if reset_n = '0' then
+      
+    end if;
+
+
 
 end architecture rtl;
